d4b3c2c71d7b01f0511758e953ae63808f75a281
Mukabony <samuel@rjoins.com>
2024-09-20 13:21:33 -0300

    Adjust config for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/nome-do-repositorio',
+  basePath: '/pactodash',
   images: {
     unoptimized: true,
   },
